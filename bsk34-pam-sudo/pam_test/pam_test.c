352146026f086f5982144a503d53de44d6b2485f
Michał Łuszczyk <michal.luszczyk@gmail.com>
2016-11-06 00:03:47 +0100

    implemented counter.

---
+++ bsk34-pam-sudo/pam_test/pam_test.c
@@ -0,0 +1,35 @@
+#include <security/pam_appl.h>
+#include <security/pam_misc.h>
+#include <stdio.h>
+ 
+static struct pam_conv login_conv = {
+  misc_conv,               /* przykładowa funkcja konwersacji z libpam_misc */
+  NULL                        /* ewentualne dane aplikacji (,,domknięcie'') */
+};
+ 
+void main () {
+  pam_handle_t* pamh = NULL;
+  int retval;
+  char *username = NULL;
+ 
+  retval = pam_start("login", username, &login_conv, &pamh);
+  if (pamh == NULL || retval != PAM_SUCCESS) {
+    fprintf(stderr, "Error when starting: %d\n", retval);
+    exit(1);
+  }
+ 
+  retval = pam_authenticate(pamh, 0);  /* próba autoryzacji */
+  if (retval != PAM_SUCCESS) {
+    fprintf(stderr, "Chyba się nie udało!\n");
+    const char *message = pam_strerror(pamh, retval);
+    printf("error %d: %s\n", retval, message);
+    exit(2);
+  }
+  else
+    fprintf(stderr, "Świetnie Ci poszło.\n");
+ 
+  printf("OK\n");  /* rzekomy kod aplikacji */
+ 
+  pam_end(pamh, PAM_SUCCESS);
+  exit(0);
+}
